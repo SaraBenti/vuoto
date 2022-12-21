9d45a04fde80fa07ec8f54981e76bde3e5e8d9a3
SaraBenti <sara.bentivegni@enaipriminifad.org>
2022-12-21 12:58:52 +0100

    esercizi if

---
+++ istruzioni/if.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IF</title>
+</head>
+
+<body>
+    <?php
+    //non posso scrivere condizioni se non sono tra parentesi
+    if (true) { //olo la condizione tru viene presa in considerazione
+        echo "condizione vera";
+    } else {
+        echo "condizione falsa";
+    }
+
+    echo "<br>";
+    $x = 1;
+    $y = 2;
+
+    if ($x > $y) {
+        //condizione vera
+        $m = $x;
+    } else {
+        //condizione falsa
+        $m = $y;
+    }
+    echo "il max tra $x e $y è $m";
+
+    echo "<br>";
+    //se fossero uguali i numeri segnerebbe l'else
+// else è opzionale
+    $a = 3;
+    $b = 3;
+    if ($a >= $b) {
+        //se l'interprete è qui significa che a maggiore o uguale a b
+        if ($a==$b){
+            echo "a uguale a b <br>";
+        }
+        echo "condizione vera";
+        $m = $a;
+    } else {
+        echo "condizione falsa";
+        $m = $b;
+    }
+        ?>
+
+
+</body>
+
+</html>
--- variabili/operazioni.php
+++ variabili/operazioni.php
@@ -54,9 +54,33 @@ echo "prima a = $a e b = $b <br>";
     $temp = $a;
     $a = $b;
     $b = $temp;
-echo "dopo a = $a e b = $b <br>"
+    echo "dopo a = $a e b = $b <br>";
+    echo "<br>";
+    //operatore ++ e -- : il ++ coincide con l'incremento di un'unità di una singola variabile (e non 2 variabili come per il +)
+echo "prima a = $a <br>";
+   // $a++; //$a = $a+1   $a++ restituisce a e POI lo incrementa; ++$a prima incrementa a e restituisce il valore già incrementato 
+    echo "dopo a = ". $a++;
+    echo "a = $a";
+    echo "<br>";
 
+    echo "dopo a = " . ++$a;
+    echo "a = $a";
+    echo "<br>";
+    //operatori logici uniscono due valori logici cioè danno un risultato booleano; è vero se è vera la prima e la seconda condizione 
+    $x = 100;
+    $compreso = ($x >= $a) && ($x <= $b); //si può usare anche AND
+    echo $compreso ? "x compreso" : "x non compreso"; //$compreso è una variabile che non uso più e quindi posso fare → (($x >= $a)AND ($x <= $b))? "x compreso" : "x non compreso";
 
+    echo "<br>";
+    //OR ||
+    $nome1 = "mario";
+    $nome2 = "giuseppe";
+    $nome = "Mario";
+
+    $nome_corretto = (($nome == $nome1) or ($nome == $nome2)) ? "nome corretto" : "nome non corretto";
+    echo $nome_corretto;
+  
+    
     ?>
 
 
