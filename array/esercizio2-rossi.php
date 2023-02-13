720d5ef27c48faca93134ef5a8889e72b92857c3
Sara Benti <sara.bentivegni@enaipriminifad.org>
2023-02-13 22:26:53 +0100

    ex

--- array/esercizio2-rossi.php
+++ array/esercizio2-rossi.php
@@ -13,23 +13,106 @@ $domanda2=['categoria'=>"assistenza", 'risposta_corretta'=>1];
 $domanda3=['categoria'=>"assistenza", 'risposta_corretta'=>0];
 $domande=[$domanda1, $domanda2, $domanda3];
 
-//copio le categorie in un array
+// 1. elencare le categorie delle domande (senza ripetizione!)
+// uso array_unique per ripulirmi un array con tutte le chiavi che mi creo io
+//copio le categorie in un array nuovo
+    $elenco_categorie=array();
+    foreach($domande as $domanda){
+        //inserisco il corso in un array
+        $elenco_categorie[]=$domanda['categoria'];
+    }
+//rimuovo le categorie duplicate
+    $elenco_categorie=array_unique($elenco_categorie);
+//-----------------------------------------------------------------------------------------
+
+// 3. definire la funzione che ha come parametro d'ingresso l'array $domande e restituisce il
+// nome della categoria più utilizzata;
 
 
 /**
- * restituisco la categoria più utilizzata
+ * restituisce la categoria più utilizzata
  * @param array $domande
  * @return string
  */
-$elenco_categorie=array();
-function most_used($domande)
-foreach($domande as $domanda){
-    if(key_exists($domanda['categoria'],$elenco_categoria)){
-        
+
+    function most_used($domande){
+        //invece che usare array_unique copio le categorie in un array
+        //inizializzo l'array
+        $elenco_categorie=array();
+
+        foreach($domande as $domanda){
+        //verifico se la domanda è già presente nell'array
+        if(key_exists($domanda['categoria'],$elenco_categorie)){
+            //la categoria esiste già, aumento i partecipanti
+            $elenco_categorie[$domanda['categoria']]++;        
+    }else{
+        //la categoria non esiste, lo aggiungo all'array e inizializzo le domande a 1
+        $elenco_categorie[$domanda['categoria']]=1;
     }
 }
+    //cerco la categoria che ha più domande
+    $massimo=0;
+    foreach($elenco_categorie as $key=>$value){
+        if($value>$massimo){
+            //il numero di domande è superiore al massimo, segno i valori
+            $massimo=$value;
+            $risultato=$key;
+        }
+    }
+    return $risultato;
+}
 
+//richiamo la funzione
+echo $risultato=most_used($domande);
+echo "<br>";
+//-----------------------------------------------------------------------------------------------
 
+// 4. modificare la funzione del punto precedente in modo che restituisca anche il numero di volte
+// che è stata utilizzata la categoria (N.B. una funzione può restituire 2 valori solo restituendo
+// un array)
+/**
+ * Restituisce la categoria più utilizzata e il numero di volte che è stata utilizzata
+ * 
+ * @param array $domande
+ * @return array $risultato
+ */
+
+ function most_used_2($domande){
+    //inizializzo l'array
+    $elenco_categorie=array();
+
+    foreach($domande as $domanda){
+        //verifico se il corso è già presente nell'array
+        if(key_exists($domanda['categoria'],$elenco_categorie)){
+            //la categoria esiste già, aumento i partecipanti
+            $elenco_categorie[$domanda['categoria']]++;
+        }else{
+            //la categoria non esiste, lo aggiungo all'array e inizializzo a 1
+            $elenco_categorie[$domanda['categoria']]=1;
+        }
+    }
+//cerco la categoria che ha più domande
+    $massimo=0;
+    foreach($elenco_categorie as $key=>$value){
+        if($value>$massimo){
+            //il numero di domande è superiore al massimo, segno i valori
+            $massimo=$value;
+            $risultato['valore']=$massimo;
+            $risultato['categoria']=$key;
+        }
+    }
+    return $risultato;
+ }
+ var_dump(most_used_2($domande));
+ echo "<br>";
+ //--------------------------------------------------------------------------------------------------
+
+// 5. se ogni risposta alle domande costasse all'azienda 50 centesimi di euro, quanto sarebbe
+// costato rispondere alle domande? (scrivere i numeri nel formato corretto!)
+// basta moltiplicare x0,5
+
+ $costo=0.5*count($domande);
+ echo "l'azienda ha speso ".$costo;
 ?>
 </body>
 </html>
--- array/esercizio_con_rossi.php
+++ array/esercizio_con_rossi.php
@@ -38,21 +38,24 @@ Svolgere le stesse attività richieste sopra (escluso il punto 6) con questi arr
     $p2=["Giannini Gianni", "Stefanini Stefano","Mariolini Maria", "Giacomini Giacomo"];
     $p3=["Violetti Viola", "Gaietti Gaia"];
     $partecipanti = [$p1, $p2, $p3];
-    var_dump($partecipanti);
-    echo $partecipanti[0][0]."<br>";
-    echo $partecipanti[1][0]."<br>";
+    //var_dump($partecipanti);
+    //echo $partecipanti[0][0]."<br>";
+    //echo $partecipanti[1][0]."<br>";
     $corsi=["Autocad", "Informatica", "Inglese"];
-    var_dump($partecipanti);
-
-//cerco l'indice dell'array relativo al corso di informatica
+    //var_dump($partecipanti);
+//---------------------------------------------------------------------------------------------------------------------------------
+//cerco l'indice dell'array relativo al corso di informatica per aggiungere novella in caso non sapessi a quale indice corrisponde
     for($i=0; $i<3;$i++){
-        if($corsi[$i]== 'Informatica')
-        $indice=$i;
+        if($corsi[$i]== 'Informatica')//perchè so che $corsi e $partecipanti hanno lo stesso indice
+        $indice=$i;//memorizza nella variabile $indice il valore della chiave 'informatica'
     }
     echo '$indice vale '. $indice."<br>";
+//aggiungo novella con l'indice trovato
+//$partecipanti[$indice][] = "Novella Novellini";
 
 //aggiungere Novella Novellini al corso di informatica
     $partecipanti[1][]= 'Novella Novellini';
+//--------------------------------------------------------------------------------------------------------------------------------
 
 //elenco di tutti i partecipanti di tutti i corsi
     foreach($partecipanti as $array){
@@ -60,17 +63,20 @@ Svolgere le stesse attività richieste sopra (escluso il punto 6) con questi arr
             echo $studente ."<br>";
         }
     }
+//-----------------------------------------------------------------------------------------------------------------------------------
 
 //calcolare il numero medio dei partecipanti ai corsi
 //calcolo il numero dei corsi
-    $numero_corsi=count($partecipanti); //è uguale a count($corsi)
+    $numero_corsi=count($partecipanti); //è uguale a count($corsi) e conta il numero degli elementi(3)
 //calcolo il numero dei partecipanti
     $numero_partecipanti=0;
     foreach($partecipanti as $array){
-        $numero_partecipanti+= count($array);
+        $numero_partecipanti+= count($array);//sommo ad ogni ciclo il numero di partecipanti
     }
 //calcolo il numero medio di partecipanti ai corsi
     $numero_medio=$numero_partecipanti/$numero_corsi;
+    echo "la media dei partecipanti ai corsi è ".$numero_medio."<br>";
+//----------------------------------------------------------------------------------------------------------------------------------
 
 //indicare il nome del corso con il maggior numero di partecipanti
     $massimo=0;
@@ -78,51 +84,57 @@ Svolgere le stesse attività richieste sopra (escluso il punto 6) con questi arr
         //il numero di partecipanti è superiore al massimo, segno i valori
         if (count($partecipanti[$i])>$massimo){
             $massimo=count($partecipanti[$i]);
-            $corso_massimo=$i;
+            $corso_massimo=$i;//ad ogni ciclo devo segnare la $i
         }
     }
     echo "il corso con più iscritti è ".$corsi[$corso_massimo];
+//----------------------------------------------------------------------------------------------------------------------------------
 
     $s1= ['cognome'=>"Primini",'nome'=>"Primo",'corso'=>"Autocad"];
     $s2 = ['cognome' => "Secondini", 'nome' => "Secondo", 'corso' => "Autocad"];
     $s3 = ['cognome' => "Giannini", 'nome' => "Gianni", 'corso' => "Informatica"];
     $studenti = [$s1, $s2, $s3];
     echo "<br>";
-    var_dump($studenti);
-    echo $studenti[0]['nome']."<br>";
+    //var_dump($studenti);
+    echo $studenti[0]['nome']." ".$studenti[0]['cognome']."<br>";
     echo $studenti[1]['nome']."<br>";
 
-//aggiungere Novella Novellini al corso di informatica
+//aggiungere Novella Novellini al corso di informatica in un array associativo
     $studenti[]=['cognome' => "Novellini", 'nome' => "Novella", 'corso' => "Informatica"];
+//-----------------------------------------------------------------------------------------------------------------------------------
 
 //elenco di tutti i partecipanti di tutti i corsi(in questo caso devo andare a prendere la chiave)
     foreach ($studenti as $array){
         echo $array['cognome']. " ".$array['nome']."<br>";
     }
-  
+//--------------------------------------------------------------------------------------------------------------------------------------
+
 //calcolare il numero medio dei partecipanti ai corsi
 //calcolo il numero di corsi
     $numero_corsi=0;
     $elenco_corsi=[];
     foreach($studenti as $studente){
-//inserisco il corso in un array 
-        $elenco_corsi= $studente['corso'];
+//inserisco il corso in un array solo se non è già presente(devo gestire i duplicati)
+        $elenco_corsi[]= $studente['corso'];
     }    
 //tolgo i valori doppi
-    $elenco_corsi=array_unique($elenco_corsi);
+    $elenco_corsi= array_unique($elenco_corsi);
 //conto i corsi unici
     $numero_corsi=count($elenco_corsi);
-//conto gli studenti
+    echo "i corsi non duplicati sono numero ".$numero_corsi."<br>";
+//conto gli studenti che sono nell'array studenti
     $numero_partecipanti=count($studenti);
 //calcolo la media
     $numero_medio= $numero_partecipanti/$numero_corsi;
+//------------------------------------------------------------------------------------------------------------------------------------
 
 //indicare il nome del corso con il maggior numero di partecipanti
-  
+//dobbiamo creare un array di corsi e uno con il numero dei partecipanti ($elenco_corsi l'ho gia fatto nella media)
     $numero_studenti=array();
     foreach ($studenti as $studente){
         //verifico se il corso è già presente nell'array
         if(key_exists($studente['corso'],$numero_studenti)){
+            //gli chiedo c'è la chiave corso?
             //il corso esiste già, aumento i partecipanti
             $numero_studenti[$studente['corso']]++;
         }
@@ -131,9 +143,12 @@ Svolgere le stesse attività richieste sopra (escluso il punto 6) con questi arr
             $numero_studenti[$studente['corso']]=1;
         }      
     }
+
 //trovo il massimo
     $massimo=0;
     foreach ($numero_studenti as $key=>$value){
+        echo 'key: ' . $key . '<br>';
+        echo 'value: ' . $value . '<br>';
         if($value>$massimo){
             //il numero di partecipanti è superiore al massimo, segno i valori
             $massimo=$value;
@@ -141,7 +156,7 @@ Svolgere le stesse attività richieste sopra (escluso il punto 6) con questi arr
         }
 
 }
-echo "il corso con più iscritti". $corso_massimo;
+echo "il corso con più iscritti è ". $corso_massimo;
     ?>
 </body>
 </html>
